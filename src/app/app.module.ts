ecccc8d6a16d6c9bc76325b6a66814192fd2db13
anesti123 <anesti_andoni@yahoo.com>
2020-11-03 10:15:54 +0100

    created components footer , header , preload, page-not-foound,error  and put them on modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from "./shared/components/header/header.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { PageNotFoundComponent } from "./shared/components/page-not-found/page-not-found.component";
+
+
+const appComponents = [AppComponent, HeaderComponent, FooterComponent, PageNotFoundComponent];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [...appComponents],
   imports: [
     BrowserModule,
     SharedModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { PreloaderComponent } from './components/preloader/preloader.component';
+import { ErrorModalComponent } from './components/error-modal/error-modal.component';
 
+const sharedComponents = [ErrorModalComponent, PreloaderComponent];
 
 @NgModule({
-  declarations: [],
+  declarations: [...sharedComponents],
   imports: [CommonModule],
   exports: []
 })
-export class SharedModule {}
+export class SharedModule { }
